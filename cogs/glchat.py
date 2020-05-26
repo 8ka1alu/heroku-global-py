9858d6b17660cd9f9b1af867941dfa183f4e9c33
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-26 14:32:12 +0900

    Update glchat.py

--- cogs/glchat.py
+++ cogs/glchat.py
@@ -17,7 +17,7 @@ class global_chat(commands.Cog):
 
         conn=r.connect()
         sm=conn.smembers("gloch")
-        sms=str(sm)
+        sms=int(sm)
         if message.channel.id in sms:
         #発言チャンネルidがsmsに入っていたら反応
 
